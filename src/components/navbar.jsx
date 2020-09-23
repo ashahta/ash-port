fc13f2cce219346536cb8f35c8b8fa2ceb0fb604
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-09-23 18:35:25 +1200

    add page links to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,34 +6,37 @@ import {Link} from "gatsby"
 export default function NavBar() {
   const [activeItem, setActiveItem] = useState();
 
-  handleItemClick = (e, { name }) => setActiveItem(name)
+  const handleItemClick = (e, { name }) => setActiveItem(name)
 
   return (
     <Menu>
-      <Menu.Item
-        name='about'
-        active={activeItem === 'about'}
-        onClick={handleItemClick}
-      >
-        About Me
-      </Menu.Item>
-
-      <Menu.Item
-        name='projects'
-        active={activeItem === 'projects'}
-        onClick={handleItemClick}
-      >
-        Projects
-      </Menu.Item>
-
-      <Menu.Item
-        name='resume'
-        active={activeItem === 'resume'}
-        onClick={handleItemClick}
-      >
-        Resume
-      </Menu.Item>
-
+      <Link to="/about/">
+        <Menu.Item
+          name="about"
+          active={activeItem === "about"}
+          onClick={handleItemClick}
+        >
+          About Me
+        </Menu.Item>
+      </Link>
+      <Link to="/projects/">
+        <Menu.Item
+          name="projects"
+          active={activeItem === "projects"}
+          onClick={handleItemClick}
+        >
+          Projects
+        </Menu.Item>
+      </Link>
+      <Link to="/resume/">
+        <Menu.Item
+          name="resume"
+          active={activeItem === "resume"}
+          onClick={handleItemClick}
+        >
+          Resume
+        </Menu.Item>
+      </Link>
     </Menu>
   )
 
