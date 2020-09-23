ea9962bd22a7e8b14a2ffb00cadb8bb099df3883
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-09-24 03:26:33 +1200

    Add styled image to layout

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Link } from "gatsby";
+import ProfileImg from "../ProfileImg.png"
+import styled from "styled-components"
 
-
+const StyledImg = styled.img`
+  max-height: 100px;
+`
 const ListLink = (props) => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
     <Link to={props.to}>{props.children}</Link>
@@ -13,7 +17,7 @@ export default function Layout({children}) {
     <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
       <header style={{ marginBottom: `1.5rem` }}>
         <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <h3 style={{ display: `inline` }}>Ash AhTa</h3>
+          <StyledImg src={ProfileImg} />
         </Link>
         <ul style={{ listStyle: `none`, float: `right` }}>
           <ListLink to="/about/">About</ListLink>
