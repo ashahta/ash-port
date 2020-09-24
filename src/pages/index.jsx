44240a0ca02043b7fd01a2b29abbe652c236a140
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-09-24 23:47:46 +1200

    import Dimmed & Hover components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../components/layout";
+import Dimmed from "../components/dimmed"
+import Hover from "../components/hover"
+import "semantic-ui-css/semantic.min.css"
 
 export default function Home() {
   return (
     <Layout>
-      <h2>Ashleigh Ah Ta</h2>
+      <Hover />
+      <Dimmed />
     </Layout>
   )
 }
