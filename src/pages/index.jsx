2e13d99ec605ab14985fd2b89952042cf4457df0
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-09-25 03:37:39 +1200

    remove dimmed and layout components

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Layout from "../components/layout";
-import Dimmed from "../components/dimmed"
 import Hover from "../components/hover"
 import "semantic-ui-css/semantic.min.css"
 
 export default function Home() {
   return (
-    <Layout>
       <Hover />
-      <Dimmed />
-    </Layout>
   )
 }
