638bfb014b7712b0716c183d591a494b68f6e513
Ashleigh Ah Ta <ashleigh.ahta@gmail.com>
2020-09-24 23:49:28 +1200

    remove page links, add styling

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -1,31 +1,22 @@
 import React from "react";
-import { Link } from "gatsby";
-import ProfileImg from "../ProfileImg.png"
 import styled from "styled-components"
 
-const StyledImg = styled.img`
-  max-height: 100px;
+const StyledDiv = styled.div`
+    margin: 3rem auto;
+    maxWidth: 650;
+    padding: 0 1rem
 `
-const ListLink = (props) => (
-  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
-  </li>
-)
+
+// const ListLink = (props) => (
+//   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+//     <Link to={props.to}>{props.children}</Link>
+//   </li>
+// )
 
 export default function Layout({children}) {
   return (
-    <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
-      <header style={{ marginBottom: `1.5rem` }}>
-        <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-          <StyledImg src={ProfileImg} />
-        </Link>
-        <ul style={{ listStyle: `none`, float: `right` }}>
-          <ListLink to="/about/">About</ListLink>
-          <ListLink to="/projects/">Projects</ListLink>
-          <ListLink to="/resume/">Resume</ListLink>
-        </ul>
-      </header>
+    <StyledDiv>
       {children}
-    </div>
+    </StyledDiv>
   )
 }
